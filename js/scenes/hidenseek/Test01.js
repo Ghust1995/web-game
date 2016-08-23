d61c4664d43e8eda9c3879cc46124ca6f6e64e4c
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-08-23 01:35:07 -0300

    Raycast on test scene now shows game objects name

--- js/scenes/hidenseek/Test01.js
+++ js/scenes/hidenseek/Test01.js
@@ -148,18 +148,19 @@ module.exports = {
         components: {
           Crosshair: {
             raycaster: new THREE.Raycaster(),
-            thingsToTest: null,
+            goToTest: null,
             init: function(go) {
-              this.thingsToTest = go.parent.parent.children;
+              this.goToTest = go.parent.parent;
             },
             update: function (go, deltaTime) {
               this.raycaster.setFromCamera( new THREE.Vector2(Input.Mouse.position.x, Input.Mouse.position.y), go.components.Camera.ref );
-              var intersects = this.raycaster.intersectObjects( this.thingsToTest, true );
+              var intersects = this.raycaster.intersectObjects( this.goToTest.children, true );
               if(intersects.length > 0){
-                var hitScan = intersects[0];
-                hitScan.object.material.color.set(0xFF0000);
-                if(_.startsWith(hitScan._nameid, "playerNetwork_"))
-                  console.log(hitScan._nameid);
+                // The mesh will always be a children of a game object
+                var hitScanned = this.goToTest.getObjectById(intersects[0].object.id).parent;
+                if(_.hasIn(hitScanned.components.Hitscan, 'onHitScan')) {
+                  hitScanned.components.Hitscan.onHitScan(hitScanned);
+                }
               }
             }
           },
@@ -250,6 +251,51 @@ module.exports = {
                       }
                     }
                   ),
+                  Hitscan: {
+                    init: function (go) {
+                      this.canvas = document.createElement('canvas');
+                    	this.context = this.canvas.getContext('2d');
+                    	this.context.font = "Bold 20px Arial";
+                    	this.context.fillStyle = "rgba(0,0,0,0.95)";
+
+                    	this.texture = new THREE.Texture(this.canvas);
+                    	this.texture.needsUpdate = true;
+
+                    	var spriteMaterial = new THREE.SpriteMaterial( { map: this.texture } );
+
+                    	this.sprite = new THREE.Sprite( spriteMaterial );
+                    	this.sprite.scale.set(200,100,1.0);
+                    	this.sprite.position.set( 0, 0, 0 );
+                    	go.add( this.sprite );
+
+                    },
+                    update: function(go, deltaTime) {
+                      if(!this.isHit && this.wasHit) {
+                				this.context.clearRect(0,0,300,300);
+                				this.texture.needsUpdate = true;
+                			}
+                      this.wasHit = this.isHit;
+                      this.isHit = false;
+                    },
+                    isHit: false,
+                    wasHit: false,
+                    onHitScan: function(go) {
+                      if ( !this.isHit && !this.wasHit ){
+                			  this.context.clearRect(0,0,640,480);
+                				var message = go._nameid;
+                				var metrics = this.context.measureText(message);
+                				var width = metrics.width;
+                				this.context.fillStyle = "rgba(0,0,0,0.95)"; // black border
+                				this.context.fillRect( 0,0, width+8,20+8);
+                				this.context.fillStyle = "rgba(255,255,255,0.95)"; // white filler
+                				this.context.fillRect( 2,2, width+4,20+4 );
+                				this.context.fillStyle = "rgba(0,0,0,1)"; // text color
+                				this.context.fillText( message, 4,20 );
+                				this.texture.needsUpdate = true;
+                			}
+                      this.isHit = true;
+                    }
+                  }
                 }, go);
 
             BaseGameObject.components.ServerNetworkTransform = {
