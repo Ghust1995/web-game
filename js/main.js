f5fb43b31f48b243c6759bc94ce70566e14c5036
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-09-15 13:43:33 -0300

    moved configuration to another file

--- js/main.js
+++ js/main.js
@@ -9,22 +9,8 @@ const Detector = require('../libraries/Detector');
 // Internal modules
 const Engine = require('./engine/Engine');
 
-// Put global initial specifications here
-
-const CONFIGURATION = {
-  INIT_SPECS: {
-    SCREEN_WIDTH: 800,
-    SCREEN_HEIGHT: 600,
-    SHOW_STATS: true,
-    CONTAINER_NAME: "WebGLContainer"
-  },
-  FIREBASE: {
-    apiKey: "AIzaSyAqHGwQN2J5BHniiZG0RtrFMHmQRDAKWCQ",
-    authDomain: "multiplayer-2108d.firebaseapp.com",
-    databaseURL: "https://multiplayer-2108d.firebaseio.com",
-    storageBucket: "multiplayer-2108d.appspot.com",
-  },
-};
+// Configuration
+const CONFIGURATION = require('./engine-config');
 
 // TODO? Populate this at runtime somehow
 const THINGS_TO_LOAD = [
